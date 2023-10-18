0c2dedc050030704b1b2ff7990504831bf5f1eb2
Daniela Camargo <daniela.camargopr@gmail.com>
2023-10-17 22:26:27 -0500

    reset

--- src/pages/api/openAIChat.js
+++ src/pages/api/openAIChat.js
@@ -4,15 +4,10 @@ const openai = new OpenAI({
   organization: process.env.NEXT_PUBLIC_OPENAI_ORGANIZATION,
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
-// const openai = new OpenAIApi(configuration);
 export default async function handler(req, res) {
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: req.body.messages,
   });
-
-  // console.log(completion.choices[0].message.content)
-
-
   res.status(200).json(completion.choices[0].message.content);
 }
