ebc74b16ca0d10c22b2a8acce283b6532c8204bb
Daniela Camargo <daniela.camargopr@gmail.com>
2023-10-18 10:10:19 -0500

    clean code

--- src/utils/parsebookopt.js
+++ src/utils/parsebookopt.js
@@ -2,14 +2,11 @@ function parseBookOptions(content) {
     const rows = content.split("\n").slice(4, 6);
     const books = rows.map((row) => {
       const columns = row.split("|").slice(1, -1);
-      const image = "http" + columns[4].trim().split("http")[1];
-      console.log(image);
-  
+     console.log(columns)
       return {
         name: columns[0].trim(),
         description: columns[1].trim(),
         language: columns[3].trim(),
-        imageLink: image,
       };
     });
     return books;
